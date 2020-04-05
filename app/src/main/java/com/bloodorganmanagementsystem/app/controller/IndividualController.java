1c2cc44c03363479c2dc0fd67ff5687b1f746a18
JeremyDsilva <jeremydsilva7@gmail.com>
2020-04-06 03:08:13 +0400

    added services to controllers

--- app/src/main/java/com/bloodorganmanagementsystem/app/controller/IndividualController.java
+++ app/src/main/java/com/bloodorganmanagementsystem/app/controller/IndividualController.java
@@ -1,23 +1,25 @@
 package com.bloodorganmanagementsystem.app.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bloodorganmanagementsystem.app.entities.Individual;
+import com.bloodorganmanagementsystem.app.service.IndividualService;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bloodorganmanagementsystem.app.entities.Individual;
-import com.bloodorganmanagementsystem.app.repository.IndividualRepository;
 
 
 @RestController
 @RequestMapping("/Individual")
 public class IndividualController {
 	
-	@Autowired
-	IndividualRepository individualRepository;
+	private final IndividualService individualService;
+
+	public IndividualController(IndividualService individualService){
+		this.individualService = individualService;
+	}
 	
 	@RequestMapping()
 	public String getIndividual(String individualId) {
-		Individual individual=individualRepository.findById("I0001").get();
+		Individual individual = individualService.findById("I0001").get();
 	
 		return individual.getFirstName();
 	}
--- app/src/main/java/com/bloodorganmanagementsystem/app/controller/TestController.java
+++ app/src/main/java/com/bloodorganmanagementsystem/app/controller/TestController.java
@@ -1,8 +1,5 @@
 package com.bloodorganmanagementsystem.app.controller;
 
-//package com.bloodorganmanagementsystem.app.webservices.controller;
-
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
