15bbfc92f4de66bfef4256820e48fee5a6461ced
JeremyDsilva <jeremydsilva7@gmail.com>
2020-04-06 03:08:00 +0400

    added annotations for spring

--- app/src/main/java/com/bloodorganmanagementsystem/app/AppApplication.java
+++ app/src/main/java/com/bloodorganmanagementsystem/app/AppApplication.java
@@ -2,8 +2,15 @@ package com.bloodorganmanagementsystem.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
+@EntityScan(basePackages = {"com.bloodorganmanagementsystem.app.entities"})
+@ComponentScan(basePackages = {"com.bloodorganmanagementsystem.app.service"})
+@ComponentScan(basePackages = {"com.bloodorganmanagementsystem.app.controller"})
+@ComponentScan(basePackages = {"com.bloodorganmanagementsystem.app.configration"})
+@EnableJpaRepositories(basePackages = {"com.bloodorganmanagementsystem.app.repository"})
 @SpringBootApplication
 public class AppApplication {
 
