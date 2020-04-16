7a7c236e49afb0a3ceea28f82143261c4bf2d3c3
sarahmohd <62908701+sarahmohd@users.noreply.github.com>
2020-04-16 20:13:04 +0400

    Update HealthOrganizationService.java

--- app/src/main/java/com/bloodorganmanagementsystem/app/service/HealthOrganizationService.java
+++ app/src/main/java/com/bloodorganmanagementsystem/app/service/HealthOrganizationService.java
@@ -1,11 +1,37 @@
 package com.bloodorganmanagementsystem.app.service;
+import java.util.List;
+import java.util.Optional;
 
-public class HealthOrganizationService {
-    //register
-    //login
-    // view List of Donors by entity
-    // view all donors created
-    // view all enities donated
-    // view all entites received as donation
-    //
+import com.bloodorganmanagementsystem.app.dto.Donation;
+import com.bloodorganmanagementsystem.app.dto.IndividualDetails;
+import com.bloodorganmanagementsystem.app.dto.Profile;
+import com.bloodorganmanagementsystem.app.entities.Individual;
+import com.bloodorganmanagementsystem.app.entities.Tests;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface HealthOrganizationService {
+ 
+ 
+    public Optional<HealthOrganization> findById(String findId); 
+
+    public boolean Register(HealthOrganization healthOrganization) throws AppException;
+
+    public boolean Login(String email, String password)throws AppException;
+
+    public List<DonationEntityDetails> viewDonationsEntities() ; // display name of donations type 
+
+    public List<Individual> viewAllDonorbyTypr( String donation_type );
+
+    public List<Individual> viewAllDonor( ); // diplay donations
+
+
+    //register --
+    //login--
+    // view List of Donors by entity --
+    // view all donors created--
+    // view all enities donated-- 
+    // view all entites received as donation--
+    
 }
