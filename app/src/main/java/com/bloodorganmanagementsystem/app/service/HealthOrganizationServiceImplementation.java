e90dd16c570847f7fc4355d5b86af3c13084648c
sarahmohd <62908701+sarahmohd@users.noreply.github.com>
2020-04-18 14:04:03 +0400

    Revert "Update HealthOrganizationServiceImplementation.java"
    This reverts commit 9f0825434704b7272941d163213b8e48f11b08f0.

--- app/src/main/java/com/bloodorganmanagementsystem/app/service/HealthOrganizationServiceImplementation.java
+++ app/src/main/java/com/bloodorganmanagementsystem/app/service/HealthOrganizationServiceImplementation.java
@@ -1,44 +1,4 @@
 package com.bloodorganmanagementsystem.app.service;
-import java.util.List;
-import java.util.Optional;
-
-import com.bloodorganmanagementsystem.app.dto.Donation;
-import com.bloodorganmanagementsystem.app.dto.IndividualDetails;
-import com.bloodorganmanagementsystem.app.dto.Profile;
-import com.bloodorganmanagementsystem.app.entities.Individual;
-import com.bloodorganmanagementsystem.app.entities.Tests;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface HealthOrganizationService {
- 
- 
-    public Optional<HealthOrganization> findById(String findId); 
-
-    public boolean Register(HealthOrganization healthOrganization) throws AppException;
-
-    public boolean Login(String email, String password)throws AppException;
-
-    public List<DonationEntityDetails> viewDonationsEntities() ; // display name of donations type 
-
-    public List<Individual> viewAllDonorbyTypr( String donation_type );
-
-    public List<Individual> viewAllDonor( ); // diplay donations
-
-
-    //register --
-    //login--
-    // view List of Donors by entity --
-    // view all donors created--
-    // view all enities donated-- 
-    // view all entites received as donation--
-    
-}
-
----
-
-package com.bloodorganmanagementsystem.app.service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -147,3 +107,22 @@ public class HealthOrganizationServiceImplementation implements HealthOrganizati
         return null;
     }
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
